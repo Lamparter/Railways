03c1174c0dd88d8bdd18c23ea417673d15656ca6
Lamparter <71598437+Lamparter@users.noreply.github.com>
2025-06-03 19:02:07 +0100

    Add initial implementation

---
+++ src/Riverside.Railways/Result`1.Conversions.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Riverside.Railways;
+
+partial struct Result<T>
+{
+	public TypeCode GetTypeCode()
+	=> Type.GetTypeCode(typeof(T));
+
+	public bool ToBoolean(IFormatProvider provider)
+		=> Status;
+
+	public byte ToByte(IFormatProvider provider)
+	{
+		if (typeof(T) == typeof(bool))
+		{
+			return Status ? (byte)1 : (byte)0;
+		}
+		else if (typeof(T) == typeof(byte))
+		{
+			return (byte)(object)this;
+		}
+		else
+		{
+			return (byte)(Status ? 1 : 0);
+		}
+	}
+
+	public char ToChar(IFormatProvider provider)
+	=> throw new NotSupportedException();
+
+	public DateTime ToDateTime(IFormatProvider provider)
+		=> throw new NotSupportedException();
+
+	public decimal ToDecimal(IFormatProvider provider)
+		=> (decimal)ToByte(provider);
+
+	public double ToDouble(IFormatProvider provider) => throw new NotImplementedException();
+
+	public short ToInt16(IFormatProvider provider) => throw new NotImplementedException();
+
+	public int ToInt32(IFormatProvider provider) => throw new NotImplementedException();
+
+	public long ToInt64(IFormatProvider provider) => throw new NotImplementedException();
+
+	public sbyte ToSByte(IFormatProvider provider) => throw new NotImplementedException();
+
+	public float ToSingle(IFormatProvider provider) => throw new NotImplementedException();
+
+	public string ToString(IFormatProvider provider) => throw new NotImplementedException();
+
+	public object ToType(Type conversionType, IFormatProvider provider) => throw new NotImplementedException();
+
+	public ushort ToUInt16(IFormatProvider provider) => throw new NotImplementedException();
+
+	public uint ToUInt32(IFormatProvider provider) => throw new NotImplementedException();
+
+	public ulong ToUInt64(IFormatProvider provider) => throw new NotImplementedException();
+}
---
+++ src/Riverside.Railways/Result`1.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.FSharp.Core;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Riverside.Railways;
+
+/// <summary>
+/// Represents the outcome of an operation, encapsulating its success status, result value, and any associated error.
+/// </summary>
+/// <remarks>
+/// The <see cref="Result{T}"/> struct provides a standardized way to represent the result of an operation, including whether the operation succeeded (<see cref="IsSuccess"/>), the resulting value (<see cref="Value"/>), and any error information (<see cref="Error"/>).
+/// It supports implicit conversions from values, exceptions, and F# result types, making it versatile for interoperation between different paradigms.
+/// </remarks>
+/// <typeparam name="T">The type of the value contained in the result.</typeparam>
+[Serializable]
+public partial struct Result<T> : IConvertible, IEquatable<Result<T>>, IEquatable<FSharpResult<T, object>>, IComparable<Result<T>>, IComparable<FSharpResult<T, object>>
+{
+	/// <summary>
+	/// Indicates whether operation associated with this result was successful.
+	/// </summary>
+	/// <remarks>
+	/// This property is a shorthand for <see cref="Status"/> property.
+	/// </remarks>
+	public readonly bool IsSuccess => Status;
+
+	/// <summary>
+	/// Indicates whether operation associated with this result failed.
+	/// </summary>
+	/// <remarks>
+	/// This property is a shorthand for the inverse of the <see cref="Status"/> property.
+	/// </remarks>
+	public readonly bool IsFailure => !Status;
+
+	/// <summary>
+	/// The generic status of the result.
+	/// </summary>
+	/// <remarks>
+	/// <c>true</c> indicates success, <c>false</c> indicates failure.
+	/// </remarks>
+	public bool Status { get; private set; }
+
+	/// <summary>
+	/// The value of the result.
+	/// </summary>
+	public T Value { get; private set; }
+
+	/// <summary>
+	/// The error associated with the result, if any.
+	/// </summary>
+	public object? Error { get; private set; }
+
+	/// <summary>
+	/// Initializes a new instance of the <see cref="Result{T}"/> struct with the specified value.
+	/// </summary>
+	/// <param name="value">The <typeparamref name="T"/> value that the <see cref="Result{T}"/> struct will encapsulate.</param>
+	public Result(T value)
+	{
+		Value = value;
+		Status = Convert.ToBoolean(value);
+	}
+
+	/// <summary>
+	/// Initializes a new instance of the <see cref="Result{T}"/> struct with the specified F# result.
+	/// </summary>
+	/// <remarks>
+	/// This constructor allows for easy conversion from F# results (<see cref="FSharpResult{T, TError}"/>) to the <see cref="Result{T}"/> struct.
+	/// </remarks>
+	/// <param name="fsharpResult">The <see cref="FSharpResult{T, TError}"/> instance to construct from.</param>
+	public Result(FSharpResult<T, object> fsharpResult)
+	{
+		Value = fsharpResult.ResultValue;
+		Status = fsharpResult.IsOk;
+		Error = fsharpResult.ErrorValue;
+	}
+
+	/// <summary>
+	/// Initializes a new instance of the <see cref="Result{T}"/> struct with the specified exception.
+	/// </summary>
+	/// <remarks>
+	/// This constructor allows for easy encapsulation of errors or exceptions that occur during operations, providing a direct way to represent failure in the result.
+	/// </remarks>
+	/// <param name="exception">The <see cref="Exception"/> instance to construct from.</param>
+	public Result(Exception exception)
+	{
+		Value = default!;
+		Status = false;
+		Error = exception;
+	}
+
+	public override string ToString()
+	{
+		if (Status)
+			return Value!.ToString();
+		else
+			return Error!.ToString();
+	}
+}
